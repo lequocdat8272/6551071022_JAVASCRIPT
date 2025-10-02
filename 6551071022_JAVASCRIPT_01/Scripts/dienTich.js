8f9cd26edc124fe206623a00e3b25630dbb1bd6b
lequocdat8272 <lequocdat8272@gmail.com>
2025-10-02 08:37:04 +0700

    fix

--- 6551071022_JAVASCRIPT_01/Scripts/dienTich.js
+++ 6551071022_JAVASCRIPT_01/Scripts/dienTich.js
@@ -2,13 +2,13 @@ function tinhDienTichTamGiac(a, b, c) {
     if (a + b <= c || a + c <= b || b + c <= a) 
     return "Không phải tam giác hợp lệ";
 
-    let p = (a + b + c) / 2;
-    let S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    var p = (a + b + c) / 2;
+    var S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
     return S;
 }
 
-let a = 5, b = 6, c = 7;
-let ketqua = tinhDienTichTamGiac(a, b, c);
+var a = 5, b = 6, c = 7;
+var ketqua = tinhDienTichTamGiac(a, b, c);
 
 document.getElementById("ketqua").innerText = "Diện tích tam giác: " + ketqua;
 
